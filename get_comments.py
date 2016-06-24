bf857272151f8063fd6c15e5fd7092846e03a08f
Eric Carmichael <eric@ckcollab.com>
2016-06-23 20:28:56 -0700

    Create get_comments.py

---
+++ get_comments.py
@@ -0,0 +1,14 @@
+# Import reddit module
+import praw
+
+
+# Connect to reddit as our generator (reddit requires 
+# you to do this, apparently)
+reddit = praw.Reddit(user_agent='Vargas Markov Generator')
+user = reddit.get_redditor('_vargas_')
+
+with open('comments.txt', 'w', encoding='utf8') as output:
+    for comment in user.get_comments(limit=None):
+        
+        comment_text = comment.body + "\n"
+        output.write(comment_text)
